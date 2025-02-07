818e7d51203f09db501da6511f07f8b141c77768
hemanth-kumar-j <hemanth@nitimis.com>
2025-02-07 23:20:28 +0530

    Added Docstring to src/try/__init__.py

--- src/try/__init__.py
+++ src/try/__init__.py
@@ -1,2 +1,6 @@
+"""This module initializes the 'try' package and provides utility functions."""
+
+
 def hello() -> str:
+    """Returns a greeting message from the try package."""
     return "Hello from try!"
